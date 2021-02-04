8a69fd2316a3467696014cfcdb195d0f85a3be11
Hugofgx9 <hugo.faugeroux@mmibordeaux.com>
2021-02-04 22:19:24 +0100

    new f vertex and muti cube

--- src/glsl/fShader.glsl
+++ src/glsl/fShader.glsl
@@ -1,17 +1,18 @@
 varying vec2 v_uv;
 
 uniform sampler2D u_map;
-uniform vec3 u_colorAmount;
+uniform float u_colorAmount;
 
 void main() {
 	vec4 map = texture2D(u_map, v_uv );
-	vec3 colorAmount = u_colorAmount;
+	float colorAmount = u_colorAmount;
 
-	float r = mix(0., 1., colorAmount.r);
-	float g = mix(0., 1., colorAmount.g);
-	float b = mix(0., 1., colorAmount.b);
-
-	vec3 finalColor = vec3(map.r * r, map.g * g, map.b * b);
+	vec3 finalColor = vec3(
+		1. - colorAmount * (1. - map.r), 
+		1. - colorAmount * (1. - map.g), 
+		1. - colorAmount * (1. - map.b)
+	);
+	
 
 	gl_FragColor = vec4(finalColor, 1.0);
 }
--- src/js/model.js
+++ src/js/model.js
@@ -1,10 +1,9 @@
 import * as THREE from 'three';
-import { Interaction } from 'three.interaction';
 import gsap from 'gsap';
 import vShader from '~/glsl/vShader.glsl';
 import fShader from '~/glsl/fShader.glsl';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import model from '../model/GORIN_GLTF_COLOR_2048_EXPORT.gltf';
+import model from '../model/GORIN_GLTF_COLOR_SPLIT_EXPORT.gltf';
 
 export default class Model {
 	constructor(scene, sceneCtx) {
@@ -27,22 +26,21 @@ export default class Model {
 
 				this.art = this.model.getObjectByName('3D', true);
 
-				this.art.material = new THREE.ShaderMaterial({
-					uniforms: {
-						u_map: { type: 't', value: this.art.material.map },
-						u_colorAmount: { type: 'vec3', value: {r: 0, g:0, b:0} },
-					},
-					vertexShader: vShader,
-					fragmentShader: fShader,
-					defines: {
-						// tofixed(1) tronque le nombre avec 1 nombre après la virgule
-						PR: window.devicePixelRatio.toFixed(1),
-					},
-				});
+				// this.art.material = new THREE.ShaderMaterial({
+				// 	uniforms: {
+				// 		u_map: { type: 't', value: this.art.material.map },
+				// 		u_colorAmount: { type: 'vec3', value: {r: 0, g:0, b:0} },
+				// 	},
+				// 	vertexShader: vShader,
+				// 	fragmentShader: fShader,
+				// 	defines: {
+				// 		// tofixed(1) tronque le nombre avec 1 nombre après la virgule
+				// 		PR: window.devicePixelRatio.toFixed(1),
+				// 	},
+				// });
 
 				this.model.traverse((child) => {
 					if (child.material && child.name != '3D') {
-						let firstMap = child.material.map;
 						child.material = new THREE.MeshBasicMaterial({
 							color: 0xffffff,
 							side: THREE.DoubleSide,
@@ -50,6 +48,22 @@ export default class Model {
 						});
 					}
 				});
+
+				this.art.children.forEach((a) => {
+					a.material = new THREE.ShaderMaterial({
+						uniforms: {
+							u_map: { type: 't', value: a.material.map },
+							u_colorAmount: { type: 'f', value: 0 },
+						},
+						vertexShader: vShader,
+						fragmentShader: fShader,
+						defines: {
+							// tofixed(1) tronque le nombre avec 1 nombre après la virgule
+							PR: window.devicePixelRatio.toFixed(1),
+						},
+					});
+				});
+				console.log(this.model);
 			},
 			undefined,
 			(error) => {
@@ -58,13 +72,13 @@ export default class Model {
 		);
 	}
 
-	setColorAmount(colorAmount) {
-		let elementTarget = this.art.material.uniforms.u_colorAmount.value;
+	colorArt(name) {
+		let elementTarget = this.art.getObjectByName(name).material.uniforms
+			.u_colorAmount;
 
-		gsap.to(elementTarget, 1,{
-			r: colorAmount.r,
-			g: colorAmount.g,
-			b: colorAmount.b,
+		gsap.to(elementTarget, 3, {
+			value: 1,
+			ease: 'power1.easeInOut'
 		});
 	}
 }
